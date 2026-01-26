928c2abe2fb79662a106af2e07abdb4e02f2d7ca
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-01-26 10:59:22 +0300

    feat: Реализовал игровой цикл в GameConsoleUI и обработку ввода

--- src/main/java/com/wordle/ui/GameConsoleUI.java
+++ src/main/java/com/wordle/ui/GameConsoleUI.java
@@ -1,5 +1,38 @@
 package com.wordle.ui;
 
-public class GameConsoleUI {
+import com.wordle.service.GameService;
 
+import java.util.Scanner;
+
+public class GameConsoleUI {
+    private final GameService gameService;
+    private final Scanner input = new Scanner(System.in);
+    public GameConsoleUI(GameService gameService){
+        this.gameService = gameService;
+    }
+    public void start(){
+        System.out.println("Добро пожаловать в игру Wordle!");
+        int counter = 1;
+        while(counter<=6){
+            System.out.println("Попытка " + counter + "/6.Введите слово из 5 букв:");
+            String word = input.nextLine();
+            if (!gameService.isWordValid(word)){
+                System.out.println("Ошибка: Слово должно быть из 5 латинских букв и существовать в словаре!");
+                continue;
+            }
+            String[] result = gameService.compareWords(word);
+            printStepResult(result, word);
+            if (gameService.isWin(result)){
+                System.out.println("Поздравляем! Вы угадали слово!");
+                return;
+            }
+            counter++;
+        }
+        System.out.println("Попытки закончились. Вы проиграли!");
+    }
+    public void printStepResult(String[] result, String word){
+        for (int i = 0; i < 5; i++){
+            System.out.println(word.charAt(i) + ": " + result[i]);
+        }
+    }
 }
