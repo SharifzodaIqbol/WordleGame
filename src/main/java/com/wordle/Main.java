1f531eef94dbe94b4adf1f2963cbcec3165f8a48
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-01-26 10:55:08 +0300

    v1.0

--- src/main/java/com/wordle/Main.java
+++ src/main/java/com/wordle/Main.java
@@ -1,9 +1,15 @@
 package com.wordle;
 
 import com.wordle.repository.WordRepository;
+import com.wordle.service.GameService;
+import com.wordle.ui.GameConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-        WordRepository words = new WordRepository();
+
+        WordRepository repository = new WordRepository();
+        GameService service = new GameService(repository);
+        GameConsoleUI ui = new GameConsoleUI(service);
+        ui.start();
     }
 }
