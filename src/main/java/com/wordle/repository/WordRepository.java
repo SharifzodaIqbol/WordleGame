71ce38749f7207166e1ad3346eadd8611823d711
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-01-29 11:15:18 +0300

    Заменил строковые значения константами

--- src/main/java/com/wordle/repository/WordRepository.java
+++ src/main/java/com/wordle/repository/WordRepository.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class WordRepository {
     private static final String FILE_NAME = "words.txt";
+    private static final String FILE_NOT_FOUND = "Файл " + FILE_NAME + " не найден";
+    private static final String ERROR_FILE = "Ошибка загрузки словаря";
     private final Set<String> words;
 
     public WordRepository() {
@@ -13,13 +15,13 @@ public class WordRepository {
     public Set<String> loadWordsFromFile() {
         Set<String> wordsSet = new HashSet<>();
         try (var is = getClass().getClassLoader().getResourceAsStream(FILE_NAME)) {
-            if (is == null) throw new RuntimeException("Файл " + FILE_NAME + " не найден");
+            if (is == null) throw new RuntimeException(FILE_NOT_FOUND);
             Scanner scanner = new Scanner(is);
             while (scanner.hasNextLine()) {
                 wordsSet.add(scanner.nextLine().toUpperCase());
             }
         } catch (Exception e) {
-            throw new RuntimeException("Ошибка загрузки словаря", e);
+            throw new RuntimeException(ERROR_FILE, e);
         }
         return wordsSet;
     }
